0e6afff15b197d1bb44a9a48669087940a446c7f
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-06-29 17:24:26 -0400

    added chapter 1 answers

---
+++ coding-problems/ctci-chap1.js
@@ -0,0 +1,27 @@
+// 1.1 Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
+
+const allUnique = ( str ) => str.split('').reduce( (isUnique, char, index) => (isUnique && str.indexOf(char) === index), true); // I like that with ES6 this is all one line!
+
+// 1.7 Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0
+
+function setZeros( matrix ){
+	const rowsToMakeZero = [];
+	const colsToMakeZero = [];
+	for (let row = 0; row < matrix.length; row++) {
+		for (let col = 0; col < matrix[row].length; col++) {
+			if ( !matrix[row][col] ){
+				rowsToMakeZero.push(row);
+				colsToMakeZero.push(col);
+			}
+		}
+	}
+
+	for (let row = 0; row < matrix.length; row++) {
+		for (let col = 0; col < matrix[row].length; col++) {
+			if ( rowsToMakeZero.includes( row ) || colsToMakeZero.includes( col )){
+				matrix[row][col] = 0;
+			}
+		}
+	}
+	return matrix;
+}
